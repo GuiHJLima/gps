2278984a70b5b81000234e3d0c9a625a0736881c
Guilherme Henrique Jerônimo de Lima <123502074+GuiLimaSeila@users.noreply.github.com>
2024-04-08 09:38:39 -0300

    Initial commit

---
+++ src/routes/index.jsx
@@ -0,0 +1,15 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { StatusBar } from "expo-status-bar";
+
+import StackRoutes from "./stack.routes";
+import TabRoutes from "./tab.routes";
+import DrawerRoutes from "./drawer.routes";
+
+export default function Routes() {
+  return (
+    <NavigationContainer>
+      <StackRoutes />
+      <StatusBar style="auto" />
+    </NavigationContainer>
+  );
+}
---
+++ src/screens/Profile/styles.js
@@ -0,0 +1,23 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#121015",
+        padding: 20,
+    },
+    button: {
+        backgroundColor: "#a7e9c4",
+        padding: 15,
+        borderRadius: 7,
+        alignItems: "center",
+        marginTop: 20,
+    },
+    buttonText: {
+        fontSize: 17,
+        fontWeight: "bold",
+        color: "#0a1034",
+    }
+});
+
+export default styles;
